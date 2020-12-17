d76ffd6f90370fce3075bb98077fcbb0ee84dda9
seqrs <basicqrs@163.com>
2020-12-17 17:44:31 +0800

    v01

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ads1115_i2c
 Bit-banging I2C with GPIO pins communicate with ads1115 adc chip
+
+run on nodemcu board (devkit ,esp8266, micropython)
---
+++ my_ads1117.py
@@ -0,0 +1,111 @@
+from machine import Pin
+import time
+sda=Pin(14,mode=Pin.OUT)
+scl=Pin(2,mode=Pin.OUT)
+
+def start():
+    sda=Pin(14,mode=Pin.OUT)
+    scl=Pin(2,mode=Pin.OUT)
+    sda.on()
+    scl.on()
+    time.sleep_us(2)
+    sda.off()
+    time.sleep_us(2)
+    scl.off()
+    time.sleep_us(2)
+def stop():
+    sda=Pin(14,mode=Pin.OUT)
+    scl=Pin(2,mode=Pin.OUT)
+    sda.off()
+    time.sleep_us(1)
+    scl.on()
+    time.sleep_us(2)
+    sda.on()
+    time.sleep_us(2)
+    
+def send_byte(byte):
+    sda=Pin(14,mode=Pin.OUT)
+    scl=Pin(2,mode=Pin.OUT)
+    scl.off()
+    for i in range(8):
+        if byte&0X80:
+            sda.on()
+            #print(1)
+        else:
+            sda.off()
+            #print(0)
+        byte<<=1
+        time.sleep_us(1)
+        scl.on()
+        time.sleep_us(2)
+        scl.off()
+        time.sleep_us(1)
+    sda.on()
+    sda=Pin(14,mode=Pin.IN)
+    time.sleep_us(1)
+    scl.on()
+    ack=sda.value()
+    time.sleep_us(2)
+    scl.off()
+    time.sleep_us(1)
+    if ack:
+        print('device no response!')
+    #print ('ack=',ack)
+    #return ack
+    
+def read_byte(n):
+    sda=Pin(14,mode=Pin.IN)
+    scl=Pin(2,mode=Pin.OUT)
+    buff=bytearray(n)
+    scl.off()
+    for j in range(n):
+        for i in range(8):
+            buff[j]<<=1
+            time.sleep_us(1)
+            scl.on()
+            time.sleep_us(2)
+            if(sda.value()):
+                buff[j]|=0x01
+            scl.off()
+            time.sleep_us(1)
+        sda=Pin(14,mode=Pin.OUT)
+        sda.off()
+        scl.on()
+        time.sleep_us(2)
+        scl.off()
+        sda=Pin(14,mode=Pin.IN)
+    #print(buff[0])
+    return buff
+
+# set register
+start()
+send_byte(0b10010000)
+send_byte(0b00000001)
+send_byte(0b11000100)
+send_byte(0b10000011)
+stop()
+
+
+# read register
+start()
+send_byte(0b10010000)
+send_byte(0b00000001)
+stop()
+start()
+send_byte(0b10010001)
+read_byte(2)
+stop()
+
+for i in range(1000):
+    time.sleep_ms(500)
+    # read date
+    start()
+    send_byte(0b10010000)
+    send_byte(0b00000000)
+    stop()
+    
+    start()
+    send_byte(0b10010001)
+    d=read_byte(2)
+    stop()
+    print(d[0],d[1],(d[0]*256+d[1])/32768.0*2.048000)
